6588753fbe520d186b44c0163459e7d9f841af23
hungduy123-4 <hungduy123pp@gmail.com>
2025-06-02 23:13:54 +0700

    H-Issues2

--- src/modules/videos/ui/components/video-owner.tsx
+++ src/modules/videos/ui/components/video-owner.tsx
@@ -4,6 +4,7 @@ import { UserAvatar } from "@/components/user-avatar";
 import { useAuth } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { SubscriptionButton } from "@/modules/subscriptions/ui/components/subscription-button";
+import { UserInfo } from "@/modules/users/ui/components/user-info";
 
 interface VideoOwnerProps {
     user: VideoGetOneOutput["user"];
@@ -20,11 +21,16 @@ VideoOwnerProps) => {
             <Link href={`/users/${user.id}`}>
                 <div className="flex items-center gap-3 min-w-0">
                     <UserAvatar size="lg" imageUrl={user.imageUrl} name={user.name} />
-                    <UserInfo />
-                    <span className="text-sm text-muted-foreground line-clamp-1">
-                        {/** do sub */}
-                        {0} subsribers
-                    </span>
+                    <div className="flex flex-col gap-1 min-w-0">
+                        <UserInfo 
+                            size="lg"
+                            name={user.name}
+                        />
+                        <span className="text-sm text-muted-foreground line-clamp-1">
+                            {/** do sub */}
+                            {0} subsribers
+                        </span>
+                    </div>
                 </div>
             </Link>
             {clerkUserId === user.clerkId ? (
