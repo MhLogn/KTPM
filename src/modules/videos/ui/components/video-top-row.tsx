9a49e7a4552504c0d5796c18733ee2ea582f2947
hungduy123-4 <hungduy123pp@gmail.com>
2025-06-03 21:27:13 +0700

    Issues2(2)

--- src/modules/videos/ui/components/video-top-row.tsx
+++ src/modules/videos/ui/components/video-top-row.tsx
@@ -1,13 +1,37 @@
+import { useMemo } from "react";
 import { VideoGetOneOutput } from "../../types";
+import { VideoDescription } from "./video-description";
 import { VideoMenu } from "./video-menu";
 import { VideoOwner } from "./video-owner";
 import { VideoReactions } from "./video-reactions";
+import { format, formatDistanceToNow } from "date-fns";
 
 interface VideoTopRowProps {
     video: VideoGetOneOutput;
 };
 
 export const VideoTopRow = ({ video }: VideoTopRowProps) => {
+    const compactViews = useMemo(() => {
+        return Intl.NumberFormat("en", {
+            notation: "compact"
+        }).format(1234567);
+    }, []);
+
+    const expandedViews = useMemo(() => {
+        return Intl.NumberFormat("en", {
+            notation: "standard"
+        }).format(1234567);
+    }, []);
+
+    const compactDate = useMemo(() => {
+        return formatDistanceToNow(video.createdAt, { addSuffix: true });
+    }, [video.createdAt]);
+
+    const expandedDate = useMemo(() => {
+        return format(video.createdAt, "d MMMM yyyy");
+    }, [video.createdAt]);
+
+
     return (
         <div className="flex flex-col gap-4 mt-4">
             <h1 className="text-xl font-semibold">{video.title}</h1>
@@ -19,6 +43,13 @@ export const VideoTopRow = ({ video }: VideoTopRowProps) => {
                     <VideoMenu videoId={video.id} variant="secondary"/>
                 </div>
             </div>
+            <VideoDescription
+                compactViews={compactViews}
+                expandedViews={expandedViews}
+                compactDate={compactDate}
+                expandedDate={expandedDate}
+                description={video.description}
+            />
         </div>
     );
 };
