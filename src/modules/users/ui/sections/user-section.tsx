55e78879583b458b46d5811d7bdf257c63c535c9
Tminhieuuuu <minhhieutd317@gmail.com>
2025-06-08 23:26:41 +0700

    Issues4(7)

--- src/modules/users/ui/sections/user-section.tsx
+++ src/modules/users/ui/sections/user-section.tsx
@@ -3,8 +3,9 @@
 import { trpc } from "@/trpc/client";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
-import { UserPageBanner } from "../components/user-page-banner";
-import { UserPageInfo } from "../components/user-page-info";
+import { UserPageBanner, UserPageBannerSkeleton } from "../components/user-page-banner";
+import { UserPageInfo, UserPageInfoSkeleton } from "../components/user-page-info";
+import { Separator } from "@/components/ui/separator";
 
 interface UserSectionProps {
     userId: string;
@@ -12,11 +13,21 @@ interface UserSectionProps {
 
 export const UserSection = (props: UserSectionProps) => {
     return (
-        <Suspense fallback={<p>Loading...</p>}>
+        <Suspense fallback={<UserSectionSkeleton/>}>
             <ErrorBoundary fallback={<p>Error</p>}>
                 <UserSectionSuspense {...props} />
             </ErrorBoundary>
         </Suspense>
+    );
+};
+
+const UserSectionSkeleton = () => {
+    return (
+        <div className="flex flex-col">
+            <UserPageBannerSkeleton />
+            <UserPageInfoSkeleton />
+            <Separator />
+        </div>
     );
 };
 
@@ -27,6 +38,7 @@ const UserSectionSuspense = ({ userId }: UserSectionProps) => {
         <div className="flex flex-col">
             <UserPageBanner user={user} />
             <UserPageInfo user={user} />
+            <Separator />
         </div>
     )
 }
